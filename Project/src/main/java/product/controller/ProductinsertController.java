14cf707fb9fb1bd37768760b9e0e890ff4e959bc
yang <ruddnjs25@gmail.com>
2020-10-15 09:53:51 +0900

    dao 생성

--- Project/src/main/java/product/controller/ProductinsertController.java
+++ Project/src/main/java/product/controller/ProductinsertController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import product.model.ProductDAO;
+
 @Controller
 public class ProductinsertController {
 	
@@ -11,10 +13,12 @@ public class ProductinsertController {
 	final String getPage = "productinsert";
 	
 	@Autowired
-	
+	ProductDAO dao;
 	
 	@RequestMapping(command)
 	public String doAction() {
+		
+		dao.GetInsertProduct();
 		
 		return getPage;
 	}
