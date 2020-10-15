cac42bed4af4a4757c5d41dc49bd1da48b852880
joowon0902 <jjoowon0902@gmail.com>
2020-10-15 10:36:12 +0900

    폼,로그인,회원가입

--- Project/src/main/java/member/model/Member.java
+++ Project/src/main/java/member/model/Member.java
@@ -1,5 +1,51 @@
 package member.model;
 
-public class Member {
 
-}
+public class Member {
+	  private int mnum;
+	  private String mname;
+	  private String mid;
+	  private String mpw;
+	  private String memail;
+	  private String maddr;
+	  
+	  
+	public int getMnum() {
+		return mnum;
+	}
+	public void setMnum(int mnum) {
+		this.mnum = mnum;
+	}
+	public String getMname() {
+		return mname;
+	}
+	public void setMname(String mname) {
+		this.mname = mname;
+	}
+	public String getMid() {
+		return mid;
+	}
+	public void setMid(String mid) {
+		this.mid = mid;
+	}
+	public String getMemail() {
+		return memail;
+	}
+	public void setMemail(String memail) {
+		this.memail = memail;
+	}
+	public String getMpw() {
+		return mpw;
+	}
+	public void setMpw(String mpw) {
+		this.mpw = mpw;
+	}
+	public String getMaddr() {
+		return maddr;
+	}
+	public void setMaddr(String maddr) {
+		this.maddr = maddr;
+	}
+	  
+	  
+	  }
