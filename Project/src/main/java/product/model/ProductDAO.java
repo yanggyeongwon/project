0a44052ea7b616ff73e8171df45471a104ef4a6f
joowon0902 <jjoowon0902@gmail.com>
2020-10-15 10:24:12 +0900

    no message

---
+++ Project/src/main/java/product/model/ProductDAO.java
@@ -0,0 +1,19 @@
+package product.model;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component("MyProduct")
+public class ProductDAO {
+	
+	private String namespace = "product.model.Product";
+	
+	@Autowired
+	SqlSessionTemplate sqlSessionTemplate;
+
+	public void GetInsertProduct() {
+		// TODO Auto-generated method stub
+		
+	}
+}
